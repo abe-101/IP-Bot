c803a7cb49567413d3ec19106847cf3b1b3a1924
Abe <abe@habet.dev>
2022-08-30 00:48:07 -0400

    add links to slacks api doc

--- ip_bot/app.py
+++ ip_bot/app.py
@@ -5,11 +5,12 @@ import re
 from slack_bolt import App
 
 # Initializes your app with your bot token and signing secret
+# https://api.slack.com/start/building/bolt-python#initialize
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"), signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-
+# https://slack.dev/bolt-python/concepts
 @app.message(
     re.compile(
         r"(?<![-\.\d])(?:0{0,2}?[0-9]\.|1\d?\d?\.|2[0-5]?[0-5]?\.){3}(?:0{0,2}?[0-9]|1\d?\d?|2[0-5]?[0-5]?)(?![\.\d])"
@@ -19,7 +20,7 @@ def say_hello_regex(say, context):
     # regular expression matches are inside of context.matches
     for ip in context["matches"]:
         # Look up IP
-        say(f"{ip} is good")
+        say(f"{ip} is amazing")
 
 
 # Start your app
